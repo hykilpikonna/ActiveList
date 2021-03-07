6727e42d0adb463f2e75bb82cade9982fbbf454d
Hykilpikonna <me@hydev.org>
2021-03-07 16:55:39 -0500

    [+] Create command

---
+++ src/main/kotlin/org/hydev/mc/ActiveList.kt
@@ -0,0 +1,30 @@
+package org.hydev.mc
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
+
+/**
+ * TODO: Write a description for this class!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2021-03-07 09:24
+ */
+class ActiveList : JavaPlugin(), CommandExecutor
+{
+    override fun onEnable()
+    {
+        super.onEnable()
+
+        getCommand("activelist").executor = this
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, _args: Array<out String>): Boolean
+    {
+        return true
+    }
+}
